78aa5b8d9c0a6fa8d3de9074c5cc098458800c52
Joaquim Vila <36737101+joaquimvila@users.noreply.github.com>
2020-05-22 19:37:51 +0200

    Added support for OpenSSL 1.1.1

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -3,12 +3,12 @@ from conans import ConanFile, CMake
 class HttpLibWebServerAdapterTestConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     generators = "cmake_find_package"
-    options = {"gtest": ["1.7.0", "1.8.1", "1.10.0"], "OpenSSL": ["1.0.2n", "1.0.2s"]}
-    default_options = {"gtest":"1.10.0", "OpenSSL":"1.0.2s"}
+    options = {"gtest": ["1.7.0", "1.8.1", "1.10.0"], "openssl": ["1.0.2n", "1.0.2s", "1.1.1g"]}
+    default_options = {"gtest":"1.10.0", "openssl":"1.1.1g"}
 
     def configure(self):
         self.options["HttpLibWebServerAdapter"].gtest = self.options.gtest
-        self.options["HttpLibWebServerAdapter"].OpenSSL = self.options.OpenSSL
+        self.options["HttpLibWebServerAdapter"].openssl = self.options.openssl
 
     def build(self):
         cmake = CMake(self)
